10876af29a439b30b0ffbed4b2eac0239fd796bd
biancamarangoni <bchagas228@gmail.com>
2021-10-13 11:16:02 -0300

    COmeço do exercicio de carros

---
+++ Exercicio/Exercicio-Carros.js
@@ -0,0 +1,27 @@
+/*
+Declare uma variável chamada `isTruthy`, e atribua a ela uma função que recebe
+um único parâmetro como argumento. Essa função deve retornar `true` se o
+equivalente booleano para o valor passado no argumento for `true`, ou `false`
+para o contrário.
+*/
+
+var isTruthy = (verdadeiroOuFalso) => verdadeiroOuFalso;
+
+// Invoque a função criada acima, passando todos os tipos de valores `falsy`.
+
+isTruthy(falsy);
+
+/*
+Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
+*/
+
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
+isTruthy(truthy);
